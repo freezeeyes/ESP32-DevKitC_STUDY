34a46a220062df004964771720b57d3c4ad89d35
freezeeyes <y.m.freelance.89.jp@icloud.com>
2020-08-31 13:38:47 +0900

    タイマー割り込みのプログラムの追加

---
+++ esp32_timer_2/esp32_timer_2.ino
@@ -0,0 +1,43 @@
+/*************************************************************
+ * TITLE: タイマー割り込みのプログラム２
+ * DESCRIPTION:
+ *     割り込み処理でシリアルモニタに文字列を表示する
+ *************************************************************/
+
+
+#include <uTimerLib.h>
+
+#define HIGH (1)
+#define LOW (0)
+#define LED_PIN 22
+
+int flag = LOW;
+int counter = 0;
+
+void toggle_led(void);
+
+void setup()
+{
+  //  LED用のピンを出力モードに指定する
+  pinMode(LED_PIN, OUTPUT);
+  //  １秒間隔のタイマー割り込みを開始する
+  TimerLib.setInterval_s(toggle_led, 1);
+}
+
+void loop()
+{
+  //  約１０秒経過したらタイマー割り込みを停止する
+  if(counter==10)
+  {
+    TimerLib.clearTimer();
+  }
+}
+
+void toggle_led(void)
+{
+  //  タイマーの経過時間をカウントする
+  counter++;
+  //  LEDの点灯状態を切り替える
+  flag = flag ? LOW : HIGH;
+  digitalWrite(LED_PIN, flag);
+}
